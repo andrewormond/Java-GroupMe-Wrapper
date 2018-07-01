83407c509a47674fc21fe57602d2028f19677fbc
Andrew Ormond <aormond@purdue.edu>
2018-07-01 17:17:35 -0400

    User Documentation

--- src/github/adeo88/groupme/api/User.java
+++ src/github/adeo88/groupme/api/User.java
@@ -1,16 +1,66 @@
 package github.adeo88.groupme.api;
 
+import java.util.Optional;
+
 import org.json.JSONObject;
 
+/**
+ * A <code>User</code> contains information about the user on a global scale in
+ * the <code>GroupMeAPI</code>.
+ * 
+ * @author adeo8
+ *
+ */
+
 public class User {
+
+	/**
+	 * The <code>user_id</code> of this <code>User</code>. Should always be present.
+	 */
 	public String user_id;
+
+	/**
+	 * The phone number associated with this <code>User</code>. May be
+	 * <code>null</code>.
+	 */
 	public String phone_number;
+
+	/**
+	 * The URL for this <code>User</code>'s Avatar. May be <code>null</code> if user
+	 * has not set an Avatar yet.
+	 */
 	public String image_url;
+
+	/**
+	 * Time this <code>User</code> was originally created in the Unix epoch format,
+	 * which is the number of seconds since January 1, 1970 in the UTC (GMT)
+	 * timezone.
+	 */
 	public long created_at;
+
+	/**
+	 * Time this <code>User</code> was last updated in the Unix epoch format, which
+	 * is the number of seconds since January 1, 1970 in the UTC (GMT) timezone.
+	 */
 	public long updated_at;
+
+	/**
+	 * The email address associated with this <code>User</code>. May be
+	 * <code>null</code>.
+	 */
 	public String email;
+
+	/**
+	 * Flag indicating whether this <code>User</code> is in sms mode.
+	 */
 	public boolean sms;
 
+	/**
+	 * Constructs the <code>User</code> object from a <code>JSONObject</code>.
+	 * 
+	 * @param <code>JSONObject</code>
+	 *            to interpret.
+	 */
 	private User(JSONObject json) {
 
 		user_id = Utils.jsonReadString(json, "id");
@@ -22,39 +72,71 @@ public class User {
 		sms = json.getBoolean("sms");
 	}
 
+	/**
+	 * Constructs a <code>User</code> associated with the application token in the
+	 * <code>GroupMeAPI</code>.
+	 * 
+	 * @param api
+	 *            The <code>GroupMeAPI<code> to use.
+	 * @return <code>User</code> corresponding to this api's token.
+	 * @throws GroupMeException
+	 *             When there is a JSON error or Networking error.
+	 */
 	public static User Me(GroupMeAPI api) throws GroupMeException {
 		JSONObject json = api.sendGetRequest("/users/me", true).getJSONObject("response");
 		return new User(json);
 	}
 
+	/**
+	 * Updates the users account with the new information provided. The avatar_url,
+	 * name, email, and zip_code can be set.
+	 * 
+	 * @param avatar_url
+	 *            Optional string in WWW URL format
+	 * @param name
+	 *            Optional string
+	 * @param email
+	 *            Optional string in an email format (Ex: tmp@fake.com)
+	 * @param zip_code
+	 *            Optional string representing the zip code
+	 * @param api
+	 *            <code>GroupMeAPI</code> containing the token of the required user.
+	 * @return HTTP status code returned from the server (200: OK)
+	 * @throws GroupMeException
+	 */
+	public static int update(Optional<String> avatar_url, Optional<String> name, Optional<String> email,
+			Optional<String> zip_code, GroupMeAPI api) throws GroupMeException {
+		JSONObject payload = new JSONObject();
+
+		if (avatar_url.isPresent()) {
+			payload.put("avatar_url", avatar_url.get());
+		}
+
+		if (name.isPresent()) {
+			payload.put("name", name.get());
+		}
+
+		if (email.isPresent()) {
+			payload.put("email", email.get());
+		}
+
+		if (zip_code.isPresent()) {
+			payload.put("zip_code", zip_code.get());
+		}
+
+		return Utils.responseToCode(api.sendPostRequest("/users/update", payload.toString(), true));
+	}
+
+	/**
+	 * Turns this <code>User</code> into a readable string.
+	 * 
+	 * @return A <code>String</code> representation of this <code>User</code>.
+	 */
 	@Override
 	public String toString() {
 		return "User [user_id=" + user_id + ", phone_number=" + phone_number + ", image_url=" + image_url
 				+ ", created_at=" + created_at + ", updated_at=" + updated_at + ", email=" + email + ", sms=" + sms
 				+ "]";
-	}
-
-	public static int update(String avatar_url, String name, String email, String zip_code, GroupMeAPI api)
-			throws GroupMeException {
-		JSONObject payload = new JSONObject();
-
-		if (avatar_url != null) {
-			payload.put("avatar_url", avatar_url);
-		}
-
-		if (name != null) {
-			payload.put("name", name);
-		}
-
-		if (email != null) {
-			payload.put("email", email);
-		}
-
-		if (zip_code != null) {
-			payload.put("zip_code", zip_code);
-		}
-
-		return Utils.responseToCode(api.sendPostRequest("/users/update", payload.toString(), true));
 	}
 
 }
